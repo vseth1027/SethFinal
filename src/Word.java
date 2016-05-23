f6e08443eeea50498cc60fc7e3c89a1dec2ada22
unknown <vseth968@FHS-LAB127-LEN9.fhs.win.fuhsd.org>
2016-05-23 08:08:22 -0700

    created Word class

---
+++ src/Word.java
@@ -0,0 +1,60 @@
+
+public class Word {
+
+	String text;
+	Integer frequency;
+	
+	public Word(String s, Integer num) {
+		text = s;
+		frequency = num;
+	}
+	
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Integer getFrequency() {
+		return frequency;
+	}
+
+	public void setFrequency(Integer frequency) {
+		this.frequency = frequency;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((frequency == null) ? 0 : frequency.hashCode());
+		result = prime * result + ((text == null) ? 0 : text.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Word other = (Word) obj;
+		if (frequency == null) {
+			if (other.frequency != null)
+				return false;
+		} else if (!frequency.equals(other.frequency))
+			return false;
+		if (text == null) {
+			if (other.text != null)
+				return false;
+		} else if (!text.equals(other.text))
+			return false;
+		return true;
+	}
+	
+	
+}
